4f414027d15302a912cc32db3a26b268791120bf
sridhar <patienceprogrammer@gmail.com>
2021-03-24 19:09:15 +0530

    printing

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ port = 587
 myEmail = os.environ.get("MAIL")
 password = os.environ.get("PASS")
 
+print(myEmail)
+print(password)
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -30,12 +33,12 @@ def contact():
             server.starttls(context=context)
             server.login(myEmail, password)
             server.sendmail(from_addr=myEmail,to_addrs=myEmail,msg=msg)
+            return render_template("success.html")
         except Exception as e:
             print("the email could not be sent.")
         finally:
-            return render_template("success.html")
             server.quit()
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
